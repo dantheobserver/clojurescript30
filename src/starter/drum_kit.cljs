57acbb93cd8c4bb53ec6ec45584687566fec6499
dantheobserver <daniel.alan.p@gmail.com>
2018-05-23 02:52:08 -0700

    Added state to get keyboard and press to work together.

--- src/starter/drum_kit.cljs
+++ src/starter/drum_kit.cljs
@@ -33,60 +33,85 @@
      :border-radius "10px"
      :transition "0.4s all"
      }
-    [:&.letter:hover :&.playing
-     {
-      :font-weight "800"
+    [:&:hover :&.playing
+     {:font-weight "800"
       :background-color "rgba(157, 197, 95, .4)"
       :transform "scale(1.1)"
       :border "3px solid #f9d275"}]
     ]
    ])
 
-(def char->str (comp str char))
-;; (def key-codes [65 83 68 70 71 72 74 75 76])
-(def key-codes [{:key 65
-                 :clip "/audio/dnb_loop.mp3"}
-                {:key 83}
-                {:key 68}
-                {:key 70}
-                {:key 71}
-                {:key 72}
-                {:key 74}
-                {:key 75}
-                {:key 76}])
+;;This probably would be better of using strings as keys.
+(def mp3 #(str "/audio/" % ".mp3"))
+(def key-codes [{:key "A" :clip (mp3 "dnb_loop") :name "backdrop"}
+                {:key "B"}
+                {:key "C"}
+                {:key "D"}
+                {:key "E"}
+                {:key "F"}
+                {:key "G"}
+                {:key "H"}
+                {:key "I"}])
 
+;; State
+(def state (r/atom {:playing #{}})) 
+(def add-playing! #(swap! state update :playing conj %))
+(def rem-playing! #(swap! state update :playing disj %))
 
-;; Helpers
-(defn- play [audio] (.play audio))
-(defn- pause [audio]
-  (.pause audio)
-  (set! (.-currentTime audio) 0.0))
+;; Pointers
+(def playing #(:playing @state))
+(defn am-i-playing? [clip]
+  (-> @(r/track playing)
+      (get clip)
+      nil?
+      not))
 
-(defn- play-pause [audio]
-  (if (.-paused audio)
-    (play audio)
-    (pause audio)))
+;; Audio Helpers
+(defn audio-cache
+  "Builds a cache of audio files from `CODES`
+  by their string key."
+  [codes]
+  (into {} (map
+            (fn [{:keys [key clip]}]
+              [key (when clip (js/Audio. clip))])
+            codes)))
+
+(defn- play-pause
+  [clip]
+  (let [playing? (not (.-paused clip))]
+    (if playing?
+      (do
+        (.pause clip)
+        (set! (.-currentTime clip) 0.0)
+        (rem-playing! clip))
+      (do
+        (.play clip)
+        (add-playing! clip)))))
+
+(defn play-pause-from-cache
+  [key cache]
+  (play-pause (cache key)))
 
 ;; Components
 (defn letter-button
   "Renders a button displaying the key
   and playing an audio clip if clicked"
-  [{:keys [key clip]} key-code]
-  (r/with-let [audio (when clip (js/Audio. clip)) ;; Keep reference to object;
-               play-handler #(play-pause audio)
-               key-handler #(when (or (= (.-charCode %) key)
-                                  (= (.-charCode %) (+ 32 key))) ;;TODO: also check for audio existence
-                          (play-handler))
-               _ (.addEventListener js/document "keypress" key-handler)]
-  [:div {:class 'letter
-         :on-click (when clip play-handler)} ;;TODO: set style when playing
-   (char->str key)]
-  (finally
-    (.removeEventListener js/document "keypress" key-handler))))
+  [{:keys [key clip]} audio]
+  (let [play-handler #(play-pause audio)
+        _ (when audio (set! (.-onended audio) #(rem-playing! audio)))]
+    (fn [_ _]
+      [:div {:class ["letter" (when @(r/track am-i-playing? audio) "playing")]
+             :on-click (when clip play-handler)} ;;TODO: set style when playing
+       key])))
 
 (defn letters []
-  [:div {:class "letter-container"}
-   (for [{:keys [key] :as kc} key-codes]
-     ^{:key key} [letter-button kc])])
+  (r/with-let [audio-clips (audio-cache key-codes)
+               key-handler #(play-pause-from-cache (.toUpperCase (.-key %)) audio-clips)
+               _ (.addEventListener js/document "keypress" key-handler)]
+    [:div {:class "letter-container"}
+     (for [{:keys [key] :as key-code} key-codes]
+       ^{:key key} [letter-button key-code (audio-clips key)])]
+    (finally
+      (.removeEventListener js/document "keypress" key-handler))))
 
 (def lesson (styled-component letters style))
